a603a8a52bffcd98ebd2f37f5ebd28ee9ede1b68
notPesol <pesol2012@hotmail.com>
2023-12-28 11:49:56 +0700

    #feat : @Transform, @ApiProperty to base SearchDTO

--- src/common/dto/search.dto.ts
+++ src/common/dto/search.dto.ts
@@ -1,41 +1,89 @@
-import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
 import { IsBoolean, IsNumber, IsString } from 'class-validator';
 
 export class SearchDTO {
   @IsBoolean()
   @Type(() => Boolean)
+  @Transform((params) => params.value === 'true')
+  @ApiProperty({
+    type: Boolean,
+    example: false,
+    required: false,
+  })
   count: boolean = false;
 
   @IsBoolean()
   @Type(() => Boolean)
+  @Transform((params) => params.value === 'true')
+  @ApiProperty({
+    type: Boolean,
+    example: false,
+    required: false,
+  })
   ignorePage: boolean = false;
 
   @IsNumber()
   @Type(() => Number)
+  @ApiProperty({
+    type: Number,
+    example: 1,
+    required: false,
+  })
   page: number = 1;
 
   @IsNumber()
   @Type(() => Number)
+  @ApiProperty({
+    type: Number,
+    example: 20,
+    required: false,
+  })
   limit: number = 20;
 
   @IsString()
   @Type(() => String)
+  @ApiProperty({
+    type: String,
+    example: '',
+    required: false,
+  })
   query: string = '';
 
   @IsString()
   @Type(() => String)
+  @ApiProperty({
+    type: String,
+    example: '',
+    required: false,
+  })
   orderBy: string = '';
 
   @IsString()
   @Type(() => String)
+  @ApiProperty({
+    type: String,
+    example: '',
+    required: false,
+  })
   orderType: 'ASC' | 'DESC' | '' = '';
 
   @IsString()
   @Type(() => String)
+  @ApiProperty({
+    type: String,
+    example: '',
+    required: false,
+  })
   between: string = '';
 
   @IsString()
   @Type(() => String)
+  @ApiProperty({
+    type: String,
+    example: '',
+    required: false,
+  })
   betweenBy: string = '';
 
   getBetweenStart() {
