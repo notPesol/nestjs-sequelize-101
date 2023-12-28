73327708b4da9853467da3802f1718b485603a85
notPesol <pesol2012@hotmail.com>
2023-12-28 10:54:48 +0700

    #feat : init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -10,6 +11,16 @@ async function bootstrap() {
   const port = process.env.PORT || 5200;
 
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Sequelize 101')
+    .setDescription('NestJs Sequelize 101 API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
